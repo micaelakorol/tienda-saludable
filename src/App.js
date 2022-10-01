8325524a4f0b38ae6582ebee9708302a90e2811d
micaelakorol <micaelakorol@gmail.com>
2022-10-01 10:51:46 -0300

    modifications-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Nav from './components/Nav'
-import UserProvider from './context/Context'
-import {Helmet} from "react-helmet";
-import './styles/App.css'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav";
+import UserProvider from "./context/Context";
+import { Helmet } from "react-helmet";
+import "./styles/App.css";
 const App = () => {
-
   return (
     <div>
-  <Helmet>
-      <link rel="icon" href={require('./img/logo.png')} />
-<title>Brotes Saludable</title>
-  </Helmet>
-<UserProvider>
-<Nav/>
-<Outlet/>
-</UserProvider>
+      <Helmet>
+        <link rel="icon" href={require("./img/logo.png")} />
+        <title>Brotes Saludable</title>
+      </Helmet>
+      <UserProvider>
+        <Nav />
+        <Outlet />
+      </UserProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
