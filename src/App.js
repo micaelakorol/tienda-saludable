5dd89c135f39853c4f5a0c20de137acca23873b1
micaelakorol <micaelakorol@gmail.com>
2022-11-14 15:36:42 -0300

    change colors

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 import UserProvider from "./context/Context";
-import { Helmet } from "react-helmet";
 import "./styles/App.css";
 const App = () => {
   return (
     <div>
-      <Helmet>
-        <link rel="icon" href={require("./img/logo.png")} />
-        <title>Brotes Productos Saludables</title>
-      </Helmet>
       <UserProvider>
         <Nav />
         <Outlet />
